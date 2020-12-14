768c64d8f31aa64f8b03b1737ae9947f50bcceb8
tientle <71688550+tientle@users.noreply.github.com>
2020-12-14 17:45:22 -0500

    Final

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ Highcharts.chart('chart', {
         text: 'Median Salaries For Science & Engineering Degree-Holders'
     },
 
-    subtitle: {
-        text: 'Source: https://ncses.nsf.gov/pubs/nsb20198/demographic-trends-of-the-s-e-workforce#women-in-the-s-e-workforce'
-    },
+    // subtitle: {
+    //     text: 'Source: https://ncses.nsf.gov/pubs/nsb20198/demographic-trends-of-the-s-e-workforce#women-in-the-s-e-workforce'
+    // },
 
     yAxis: {
         title: {
@@ -59,21 +59,16 @@ Highcharts.chart('chart', {
 
 
 // SCALE AND COLOR CHANGE //
-// Scale Animation Setup
-// .to('@target', @length, {@object})
 var scale_tween = TweenMax.to('#scale-animation', 1, {
   transform: 'scale(.90)',
   ease: Linear.easeNone
 });
 
-// BG Animation Setup
-// .to('@target', @length, {@object})
 var bg_tween = TweenMax.to('#bg-trigger', 1, {
   backgroundColor: '#d6fbff',
   // the color it changes to^
   ease: Linear.easeNone
 });
-
 
 // init ScrollMagic Controller
 var controller = new ScrollMagic.Controller();
@@ -98,16 +93,14 @@ controller.addScene([
 
 
 
-// SCROLL//
+// UP SCROLL //
 
 $(function () { // wait for document ready
 		// init
 		var controller = new ScrollMagic.Controller({
 			globalSceneOptions: {
 				triggerHook: 'onLeave',
-				duration: "200%" // this works just fine with duration 0 as well
-				// However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
-				// Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
+				duration: "200%"
 			}
 		});
 
@@ -125,29 +118,29 @@ $(function () { // wait for document ready
 		}
 	});
 
-  // SCROLL END
+// UP SCROLL END //
+// http://scrollmagic.io/examples/basic/section_wipes_natural.html //
 
-// PIN Scroll
+
+// SIDE SCROLL //
 $(function () { // wait for document ready
-  // init
+
   var controller = new ScrollMagic.Controller();
 
-  // define movement of panels
   var wipeAnimation = new TimelineMax()
-    // animate to second panel
+
     .to("#slideContainer", 0.5, {z: -150})		// move back in 3D space
     .to("#slideContainer", 1,   {x: "-25%"})	// move in to first panel
     .to("#slideContainer", 0.5, {z: 0})				// move back to origin in 3D space
-    // animate to third panel
+
     .to("#slideContainer", 0.5, {z: -150, delay: 1})
     .to("#slideContainer", 1,   {x: "-50%"})
     .to("#slideContainer", 0.5, {z: 0})
-    // animate to forth panel
+
     .to("#slideContainer", 0.5, {z: -150, delay: 1})
     .to("#slideContainer", 1,   {x: "-75%"})
     .to("#slideContainer", 0.5, {z: 0});
 
-  // create scene to pin and link animation
   new ScrollMagic.Scene({
       triggerElement: "#pinContainer",
       triggerHook: "onLeave",
@@ -157,6 +150,8 @@ $(function () { // wait for document ready
     .setTween(wipeAnimation)
     .addTo(controller);
 });
+// SIDE SCROLL END //
+// http://scrollmagic.io/examples/advanced/section_slides_manual.html
 
 
 // PARALLAX
@@ -176,3 +171,6 @@ new ScrollMagic.Scene({triggerElement: "#parallax2"})
 new ScrollMagic.Scene({triggerElement: "#parallax3"})
         .setTween("#parallax3 > div", {y: "80%", ease: Linear.easeNone})
         .addTo(controller);
+
+// PARALLAX END
+// http://scrollmagic.io/examples/advanced/parallax_sections.html //
